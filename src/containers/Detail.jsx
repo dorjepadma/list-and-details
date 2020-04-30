cb757bc8d4737e596446c803ee8b4077da0b031d
dorjepadma <dorjepadma@gmail.com>
2020-04-29 23:15:11 -0700

    working charater page rendering

--- src/containers/Detail.jsx
+++ src/containers/Detail.jsx
@@ -2,30 +2,4 @@
 import React, { useState, useEffect } from 'react';
 import { getDetail } from '../services/AirbenderDetails';
 
-const DetailsList = () => {
 
-  const [details, setDetails] = useState([]);
-
-  useEffect(() => {
-    getDetail()
-      .then(newDetails => setDetails(newDetails));
-
-  }, []);
-
-  return (
-    <>
-      {characters.map (character => 
-        <div key={character.id} className="characterContainer">
-          <img className="characterImage" 
-            src={character.photoUrl}
-            style={{ width: 250, height: 250, borderRadius: 250 / 2 }} 
-            alt={character.name}
-          />           
-          <h2 className="characterName">{character.name}</h2>
-        </div>
-      )
-      }
-    </>
-  );
-};
-export default DetailsList;
