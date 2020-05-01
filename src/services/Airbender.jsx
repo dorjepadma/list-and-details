29f14362bf7f73e70c60869dccafd9a50183bb23
dorjepadma <dorjepadma@gmail.com>
2020-05-01 16:42:51 -0700

    still working on services

--- src/services/Airbender.jsx
+++ src/services/Airbender.jsx
@@ -1,8 +1,16 @@
 //handles Airbender API call
-export const getCharacters = () => {
+export const fetchCharacters = () => {
   return fetch('http://last-airbender-api.herokuapp.com/api/v1/characters')
-    .then(res => res.json());
+    .then(res => res.json())
+    .then(json => json.results.map(({ characterId, name, allies, enemies, weapon, position, affilation, }) => ({
+      characterId,
+      name,
+      description: `This character is ${name} and is ${affiliation}`,
+      //to insure broken images are replaced by place holders. I had this issue with my earlier release. 
+      image: image.includes('Maxresdefault.jpg') ? 'https://www.fillmurray.com/640/360' :image
+    })));
 };
+
 export const getDetail = CharacterId => {
   return fetch(`http://last-airbender-api.herokuapp.com/api/v1/characters${CharacterId}`)
     .then(res => res.json())
